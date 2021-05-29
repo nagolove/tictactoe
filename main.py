d85a9a8ffd25709d84727795c2db76324ef475a6
nagolove <dekar.own@gmail.com>
2021-05-29 19:59:13 +0300

    Task description. Function got parameters.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-def fill():
+exm3 = "XOOOXOXXO"
+
+def fill(exm):
 	pole=[]
 	for i in range(0,3):
 		pole.append([])
 	counter1=0
 	counter2=0	
-	for char in exm3:
+	for char in exm:
 		if counter1%3==0:
 			counter2+=1
 			counter1=0
@@ -14,3 +16,10 @@ def fill():
 		counter1+=1
 		
 	return pole
+
+def test():
+        print("exm3", exm3)
+        pole = fill(exm3)
+        print(pole)
+
+test()
